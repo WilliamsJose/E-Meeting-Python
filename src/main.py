3753d4cc49418c992d139c89bca15193e1559045
Williams Jose <moflin02@gmail.com>
2019-11-21 11:46:11 -0300

    Refatorando código 'meudeusoqueeisso'

--- src/main.py
+++ src/main.py
@@ -15,163 +15,86 @@ def main():
 
 # <Telas>
 def principal():
-    clear()
-    print("Bem vindo!")
-    print("Qual menu deseja ver? ")
+    message("Seja bem vindo! entre ou cadastre-se.")
 
-    text = "\n(1) Usuário Comum. \n(2) Coordenador. \n(3) Gestor de Recursos. \n(0) Sair \n: "
+    text = "\n(1) Entrar. \n(2) Cadastrar-se. \n(0) Sair \n: "
     opt = input(text)
 
     if opt == "1":
-        menuUsuarioComum()
+        login()
     elif opt == "2":
-        menuCoordenador()
-    elif opt == "3":
-        menuGestorDeRecursos()
-    elif opt == "0":
-        clear()
-        print("Goodbye!")
-        sleep(1.5)
-        clear()
-        exit()
-    else:
-        print("\nOpção inválida! Tente novamente.")
-        sleep(1.5)
-        principal()
-
-
-def menuUsuarioComum():
-    clear()
-    print("--Usuário--")
-    print("Faça login ou Cadastre-se")
-
-    text = "\n(1) Cadastrar-se. \n(2) Entrar. \n(0) Voltar ao Menu Anterior \n: "
-    opt = input(text)
-
-    if opt == "1":
         cadastroComum()
-    elif opt == "2":
-        login("Comum")
     elif opt == "0":
-        principal()
+        exitProgram()
     else:
-        clear()
-        print("\nOpção inválida! Tente novamente.")
-        sleep(1.5)
-        menuUsuarioComum()
-
-
-def menuCoordenador():
-    clear()
-    print("--Coordenador--")
-    text = "(1) Entrar. \n(0) Voltar ao Menu Anterior. \n: "
-
-    opt = input(text)
-
-    if opt == "1":
-        login("Coordenador")
-    elif opt == "0":
+        message("Opção inválida! Tente novamente.")
         principal()
-    else:
-        print("\nOpção inválida, Tente novamente.")
-        sleep(1.5)
-        menuCoordenador()
 
-
-def menuGestorDeRecursos():
-    clear()
-    print("--Gestor de Recursos--")
-    text = "(1) Entrar. \n(0) Voltar ao Menu Anterior. \n: "
-
-    opt = input(text)
-
-    if opt == "1":
-        login("Gestor de Recursos")
-    elif opt == "0":
-        principal()
-    else:
-        print("\nOpção inválida, Tente novamente.")
-        sleep(1.5)
-        menuGestorDeRecursos()
 
 
 def usuarioComumLogado(u):
-    clear()
-    print("-----Tela usuário comum----- \nO que deseja fazer hoje?")
-    opt = input("(1) Cadastrar nova Reunião \n(2) Confirmar ou Negar presença \n(3) Reuniões confirmadas \n(4) Atas \n: ")
+    message("-----Tela usuário comum----- \nO que deseja fazer hoje?")
+    
+    opt = input("(1) Cadastrar nova Reunião \n(2) Confirmar ou Negar presença \n(3) Reuniões confirmadas \n(4) Atas \n(0) Sair \n: ")
 
     if opt == "1":
         cadastrarReuniao(u)
     elif opt == "2":
-        print("simOuNaoReuniao()")
-        sleep(1)
+        message("simOuNaoReuniao()") #temp
         usuarioComumLogado(u)
     elif opt == "3":
-        print("reunioesConfirmadas()")
-        sleep(1)
+        message("reunioesConfirmadas()") #temp
         usuarioComumLogado(u)
     elif opt == "4":
-        print("atas()")
-        sleep(1)
+        message("atas()") #temp
         usuarioComumLogado(u)
     elif opt == "0":
-        exit()
+        exitProgram()
     else:
-        clear()
-        print("\nOpção inválida, Tente novamente.")
-        sleep(1.5)
+        message("Opção inválida! Tente novamente.")
         usuarioComumLogado(u)
 
 
 def coordenadorLogado(u):
-    clear()
-    print("-----Tela Coordenador-----")
+    message("-----Tela Coordenador-----")
+
     opt = input("(1) Criar reunião \n(2) Editar atas \n(3) Realocar reunião \n(4) Confirmar ou Negar presença \n(5) Reuniões confirmadas \n(0) Sair \n: ")
 
     if opt == "1":
         cadastrarReuniao(u)
     elif opt == "2":
-        print("editarAta()")
-        sleep(1)
+        message("editarAta()") #temp
         coordenadorLogado(u)
     elif opt == "3":
-        print("realocarReuniao()")
-        sleep(1)
+        message("realocarReuniao()") #temp
         coordenadorLogado(u)
     elif opt == "4":
-        print("simOuNaoReuniao()")
-        sleep(1)
+        message("simOuNaoReuniao()") #temp
         coordenadorLogado(u)
     elif opt == "5":
-        print("reunioesConfirmadas()")
-        sleep(1)
+        message("reunioesConfirmadas()") #temp
         coordenadorLogado(u)
     elif opt == "0":
-        exit()
+        exitProgram()
     else:
-        clear()
-        print("\nOpção inválida, Tente novamente.")
-        sleep(1.5)
+        message("Opção inválida! Tente novamente.")
         coordenadorLogado(u)
 
 
 def gestorLogado(u):
-    clear()
-    print("-----Tela Gestor-----")
+    message("-----Tela Gestor-----")
+
     opt = input("(1) Cadastrar novo espaço \n(2) Confirmar local de Reunião \n(0) Sair \n: ")
 
     if opt == "1":
         cadNovoEspaco(u)
     elif opt == "2":
-        print("confirmarReuniao()")
-        sleep(1)
+        message("confirmarReuniao()") #temp
         gestorLogado(u)
     elif opt == "0":
-        exit()
+        exitProgram()
     else:
-        clear()
-        print("\nOpção inválida! Tente novamente.")
-        sleep(1.5)
+        message("Opção inválida! Tente novamente.")
         gestorLogado(u)
     
 
@@ -203,6 +126,23 @@ def fileWrite(db):
         print("Erro na gravação do arquivo. " + str(IOError))
 
 
+def welcome(nome):
+    clear()
+    print(f"Bem vindo(a), {nome} !")
+    sleep(1.5)
+
+def exitProgram():
+    clear()
+    print("Goodbye!")
+    sleep(1.5)
+    clear()
+    exit()
+
+def message(msg):
+    clear()
+    print(msg)
+    sleep(1.5)
+
 def cadastroComum():
     clear()
     db = fileRead()
@@ -215,7 +155,7 @@ def cadastroComum():
     while usuarioVerificado == False:
         for usuarioDB in db["usuarios"]:
             if usuario == usuarioDB["usuario"]:
-                print("Esse usuário já existe, tente outro.")
+                message("Esse usuário já existe, por favor tente outro.")
                 usuario = input("Digite seu usuario: ")
                 usuarioVerificado = False
             else:
@@ -237,10 +177,9 @@ def cadastroComum():
                       "telefone": telefone, "permissao": permissao, "senha": senha, "reunioesProprietario": [], "reunioesConfirmadas": []}
             senhaVerificada = True
         else:
+            message("Senhas devem ser iguais!\n")
             clear()
-            print("Senhas devem ser iguais!\n")
-            print("Digite seu nome completo: ", nome, "\nDigite seu usuario: ", usuario, "\nDigite seu CPF: ", cpf,
-                  "\nDigite sua Matrícula: ", matricula, "\nDigite seu Telefone: ", telefone)
+            print(f"Digite seu nome completo: {nome} \nDigite seu usuario: {usuario} \nDigite seu CPF: {cpf} \nDigite sua Matrícula: {matricula} \nDigite seu Telefone: {telefone}")
             senha = getpass("Digite a Senha: ")
             confirmaSenha = getpass("Confirme a Senha: ")
 
@@ -252,16 +191,12 @@ def cadastroComum():
         db["usuarios"].append(pessoa)
     finally:
         # Salva o db atualizado no arquivo
-        fw = open(f, "w+", encoding="utf8")
-        fw.write(json.dumps(db, ensure_ascii=False, indent=4))
-        fw.close()
-
-    print("\nCadastro Efetuado!")
-    sleep(2)
-    menuUsuarioComum()
+        fileWrite(db)
+        message("\nCadastro efetuado com sucesso!")
+        principal()
 
 
-def login(permissao):
+def login():
     clear()
     # retorno da leitura do arquivo
     db = fileRead()
@@ -269,29 +204,23 @@ def login(permissao):
     senha = getpass("Senha: ")
 
     # Verifica o tipo  de usuario e se o que foi digitado é igual ao que tenho guardado
-    if permissao == "Comum":
-        for usuarioDB in db["usuarios"]:
-            if usuario == usuarioDB["usuario"] and senha == usuarioDB["senha"]:
-                print("Bem vindo(a), " + usuarioDB["nome"] + "!")
-                sleep(1.5)
+    for usuarioDB in db["usuarios"]:
+        if usuarioDB["usuario"] == usuario and senha == usuarioDB["senha"]:
+            if usuarioDB["permissao"] == "Comum":
+                welcome(usuarioDB["nome"])
                 usuarioComumLogado(usuarioDB)
                 break
-        else:
-            print("Usuário ou Senha Inválidos")
-            sleep(2)
-            principal()
-
-    else:
-        for usuarioDB in db["usuarios"]:
-            if usuario == usuarioDB["usuario"] and senha == usuarioDB["senha"] and permissao == usuarioDB["permissao"]:
-                print("Bem vindo(a), " + usuarioDB["nome"] + "!")
-                sleep(1.5)
-                coordenadorLogado(usuarioDB) if usuarioDB["permissao"] == "Coordenador" else gestorLogado(usuarioDB)
+            elif usuarioDB["permissao"] == "Coordenador":
+                welcome(usuarioDB["nome"])
+                coordenadorLogado(usuarioDB)
                 break
-        else:
-            print("Usuário ou Senha Inválidos")
-            sleep(2)
-            principal()
+            elif usuarioDB["permissao"] == "Gestor de Recursos":
+                welcome(usuarioDB["nome"])
+                gestorLogado(usuarioDB)
+                break
+    else:
+        message("Usuário ou Senha inválidos")
+        principal()
 
 
 def cadastrarReuniao(u):
@@ -310,7 +239,7 @@ def cadastrarReuniao(u):
     
     # renderiza todas as salas de acordo com o banco, permite que o usuário escolha uma e verifica se está ocupada
     while True:
-        print("\nQual sala deseja escolher? ")
+        message("\nQual sala deseja escolher? ")
         for i, sala in enumerate(db["salas"]):
             print("("+str(i+1)+")", sala["sala"], sala["status"])
 
@@ -319,18 +248,14 @@ def cadastrarReuniao(u):
         try:
             sala = int(sala) - 1
             if db["salas"][sala]["status"] == "Ocupada":
-                print("\nEsta sala está ocupada, por favor escolha outra")
-                sleep(1.5)
-                clear()
+                message("\nEsta sala está ocupada, por favor escolha outra")
             else:
                 break
         except ValueError as e:
-            print("\nErro: {}, por favor digite um número.".format(e))
-            sleep(3)
-            clear()
+            message(f"Erro: {e}, por favor digite um número.")
+            sleep(1.5)
     
-    print("Sala escolhida: " + db["salas"][sala]["sala"])
-    sleep(1.5)
+    message("Sala escolhida: " + db["salas"][sala]["sala"])
     clear()
 
     data = input("\nDigite a data que será realizada no formato '03/08/2019' \n:")
@@ -372,13 +297,10 @@ def cadastrarReuniao(u):
                         p["presencaRequerida"] = []
                         p["presencaRequerida"].append(idReuniao)
 
-                    clear()
-                    print("Participante " + p["nome"] + " adicionado.")
-                    sleep(1.5)
+                    message(f"Participante {p['nome']} adicionado.")
                     break
             else:
-                print("Participante não existe.")
-                sleep(1.5)
+                message("Este participante não existe.")
 
     # dict, object, json, já não sei mais como chamar isso...
     reuniao = {
@@ -407,10 +329,8 @@ def cadastrarReuniao(u):
         db["reunioes"] = []
         db["reunioes"].append(reuniao)
     finally:
-        clear()
         fileWrite(db)
-        print("Reunião cadastrada com êxito! ")
-        sleep(1.5)
+        message("Reunião cadastrada com êxito! ")
         
         # volta a tela do usuario que está logado
         if usuarioLogado["permissao"] == "Comum":
@@ -431,6 +351,8 @@ def cadNovoEspaco(u):
     clear()
 
     while True:
+        clear()
+
         disponivel = input("A sala já está disponível? [S/N] \n: ").upper()
         clear()
 
@@ -441,10 +363,7 @@ def cadNovoEspaco(u):
             status = "Ocupada"
             break
         else:
-            clear()
-            print("Opção inválida, tente novamente.")
-            sleep(1.5)
-            clear()
+            message("Opção inválida! Tente novamente.")
 
     novoEspaco = {
         "sala": sala,
